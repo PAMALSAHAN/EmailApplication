6771849ff7c26437e9522afdb586f7dcf072adfd
PAMALSAHAN <43300747+PAMALSAHAN@users.noreply.github.com>
2019-07-07 23:06:05 +0530

    init randpassoword function

--- Email.java
+++ Email.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Email
  */
@@ -6,6 +8,7 @@ public class Email {
     private String firstName;
     private String lastName;
     private String password;
+    private int defaultpassword=8;
     private String department;
     private int mailboxCapasity;
     private String alternateEmail;
@@ -18,8 +21,77 @@ public class Email {
         this.firstName=fName;
         this.lastName=lName;
         System.out.println("Email was Created :"+this.firstName +" "+ this.lastName);
+        this.department=setDepartment(); //use this key word other wise get an 
+       
+        // set password
+        this.password=randomPassword(defaultpassword);
+        System.out.println(password);
+
     }
+
     // ask for the department
+    private String setDepartment(){
+        System.out.println("Enter your Department:");
+        System.out.println("1.Sales");
+        System.out.println("2.Developments");
+        System.out.println("3.Accounting");
+        System.out.println("0.for none");
+        System.out.println("Enter your code:");
+        Scanner in=new Scanner(System.in);
+        int DepCode=in.nextInt();
+        in.close(); 
+        // we can close the other wise it gives an error resources leak it is not 
+        //for when excuting.
+        //resource leak mean loss of memory needlessly
+
+
+        switch (DepCode) {
+            case 1:
+                return "Sales"; 
+              
+            case 2:
+                return "Developments";
+               
+            case 3:
+                return "Accounting";
+                
+            default:
+                return "none";
+               
+
+                //we do not have to use break because we use return key word so after 
+                //comming to the return code will terminate
+
+        }
+
+    }
+
+    // set generate random password
+    private String randomPassword(int length){
+        String passwordChar="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String passwordNum="1234567890";
+        char[] password=new char[length];
+        password[0]='#';
+        password[3]='@';
+
+        for (int i = 1; i <3; i++) {
+           int rand=(int) (Math.random()*passwordChar.length());
+           password[i]=passwordChar.charAt(rand);
+           
+        }
+        for(int j=4;j<length;j++){
+            int rd=(int) (Math.random()*passwordNum.length());
+            password[j]=passwordNum.charAt(rd);
+           
+            
+        }
+        //have to convert char array in to string so we have to 
+        String word=new String(password); // constructor eke valueOf kiyana function eka use karanawa
+        //we can use String.valueOf(password); also
+        return word; //or we can use new String(password);
+
+
+    }
 
     //set the mailbox mailboxCapasity
 
